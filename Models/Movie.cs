1d57d9ab3ec65c2f2e3b5d5bd0f62988151411d4
mark song <69584321+marksongg1981@users.noreply.github.com>
2020-08-17 10:13:46 +0800

    Initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength =3)]
+        public string Title { get; set; }
+
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
+        public string Genre { get; set; }
+        [Range(1, 100)]
+       
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+    }
+
+}
